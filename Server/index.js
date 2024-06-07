823f683a13c28be2d365fc43f95e941c68325603
Satyakarthik05 <160334326+Satyakarthik05@users.noreply.github.com>
2024-06-07 12:39:48 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -14,6 +14,7 @@ const app =express()
 app.use(express.json())
 app.use(cors({
     origin:["http://localhost:5174"],
+    methods:["POST","GET"],
     credentials:true
 }))
 app.use(cookieParser())
